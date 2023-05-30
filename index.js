ba0acabd39af56ac525b565981be5ce07fc4bf34
Agustin <agustin.gomez@leniolabs.com>
2023-05-30 18:46:39 -0300

    This is my commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Give me a lodash example
+// https://lodash.com/docs/4.17.15
+
+const _ = require('lodash');
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const chunked = _.chunk(arr, 2);
+console.log(chunked);
